f236f2be603d822b65edf35d90a1b2a3f8c8d44a
Touseef Younas <touseefyounasch@gmail.com>
2024-11-05 17:39:22 -0500

    adding login page

--- frontend-react/src/Register.tsx
+++ frontend-react/src/Register.tsx
@@ -1,44 +1,42 @@
-import { Link } from "react-router-dom"
-
 
 const Register = () => {
     return (
         <>
-        <div className=''>
+        <div>
             <div className=' w-full m-4'>
                 <div className="w-2/6 mx-auto mt-20 bg-secondary rounded-xl">
                 <form className='rounded-lg text-text  m-4 py-4 px-6 '>
-                    <label><h1 className='pb-2 text-text font-medium'>Register</h1></label>
+                    <label><h1 className='pb-2 text-text text-xl font-medium'>Register</h1></label>
                     
                     <div className='flex md:flex-wrap flex-col md:flex-row md:space-x-4'>
                     <div className='mb-5 flex-1'>
-                    <label className='block m-1 text-sm font-medium text-text dark:text-white' htmlFor='firstName'>First Name *</label>
-                    <input type="text" name="firstName" required className="block w-full p-2.5 text-sm bg-background rounded-full border border-gray-300" />
+                    <label className='block m-1 p-2 text-sm font-medium text-white' htmlFor='firstName'>First Name *</label>
+                    <input type="text" name="firstName" required className="block w-full p-2.5 text-primary bg-background rounded-full" />
                     </div>
                     <div className='mb-5 flex-1'>
-                    <label className='block m-1 text-sm font-medium text-text dark:text-white' htmlFor='lastName'>Last Name *</label>
-                    <input className="block w-full p-2.5 text-sm bg-background rounded-full border border-gray-300" type="text" name="lastName"/>
+                    <label className='block m-1 p-2  text-sm font-medium text-white' htmlFor='lastName'>Last Name *</label>
+                    <input className="block w-full p-2.5 text-primary bg-background rounded-full" type="text" name="lastName"/>
                     </div>
                     </div>
 
                     <div className='mb-5'>
-                    <label className='block m-1 text-sm font-medium text-text dark:text-white' htmlFor='email'>Email *</label>
-                    <input  type="text" name="email" autoComplete="address-line1" required className="block w-full p-2.5 text-sm bg-background rounded-full border border-gray-300"/>
+                    <label className='block m-1 p-2 text-sm font-mediumtext-white' htmlFor='email'>Email *</label>
+                    <input  type="text" name="email" autoComplete="address-line1" required className="block w-full p-2.5 text-primary bg-background rounded-full"/>
                     </div>
 
                     <div className='mb-5'>
-                    <label className='block m-1 text-sm font-medium text-text dark:text-white' htmlFor='username'>Username</label>
-                    <input type="text" name="username" autoComplete="address-line2" className="block w-full  p-2.5 text-sm bg-background rounded-full border border-gray-300"/>
+                    <label className='block m-1 p-2 text-sm font-medium text-white' htmlFor='username'>Username</label>
+                    <input type="text" name="username" autoComplete="address-line2" className="block w-full p-2.5 text-primary bg-background rounded-full"/>
                     </div>
 
                     <div className='mb-5'>
-                    <label className='block m-1 text-sm font-medium text-text dark:text-white' htmlFor='password'>Password</label>
-                    <input type="password" name="password" autoComplete="address-line2" className="block w-full  p-2.5 text-sm bg-background rounded-full border border-gray-300"/>
+                    <label className='block m-1 p-2 text-sm font-medium text-primary dark:text-white' htmlFor='password'>Password</label>
+                    <input type="password" name="password" autoComplete="address-line2" className="block w-full p-2.5 text-primary bg-background rounded-full"/>
                     </div>
 
                     <div className='mb-5'>
                     
-                    <button type='submit' className="text-text bg-accent mr-4 px-4 py-2 rounded-full min-w-[100px] hover:font-bold hover:bg-primary">Register</button>
+                    <button type='submit' className="text-text bg-accent mr-4 px-4 py-2 rounded-full w-full hover:font-bold hover:bg-primary">Register Now</button>
               
                     </div>                   
                     </form>
--- frontend-react/src/Router.tsx
+++ frontend-react/src/Router.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import App from './App'
 import Register from './Register';
+import Login from './Login';
 
 const Router = () => {
 
@@ -8,6 +9,7 @@ const Router = () => {
         <>
             <Route path='/' element={<App />}>
                 <Route path='register' element={<Register/>}/>
+                <Route path='login' element={<Login/>}/>
             </Route>
         </>
     ))
